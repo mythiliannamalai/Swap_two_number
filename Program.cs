8a90eadd714e0fb749819b16fe2e12c5f245c948
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 21:03:37 +0530

    Swap_number

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Swap_two_number
+{
+    class Program
+    {
+        public void Calulation()
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("before swap a={0} b={1}",a ,b);
+            a = (a * b);
+            b = (a/ b);
+            a= (a / b);
+            Console.WriteLine("after swap a={0} b={1}", a, b);
+        }
+    }
+    class Result
+    {
+        public static void Main(string[]args)
+        {
+            Program program = new Program();
+            program.Calulation();
+        }
+    }
+}
